421514f84c8ad3b53e0b42febfb92b4dffed7a41
riez <bao.thcs20@gmail.com>
2025-11-10 13:55:33 +0800

    feat: multiprocessing downloader

--- lib/download_wsi.py
+++ lib/download_wsi.py
@@ -1,17 +1,15 @@
-from tqdm import tqdm
+from multiprocessing import Process
+import numpy as np
 import pandas as pd
 import os
 import requests
 import re
 import argparse
 
-save_dir = "../wsi_files/BLCA"
-
 
 def download_svs(file_id, save_dir):
     os.makedirs(save_dir, exist_ok=True)
     data_endpt = "https://api.gdc.cancer.gov/data/{}".format(file_id)
-
     response = requests.get(
         data_endpt, headers={"Content-Type": "application/octet-stream"}
     )
@@ -25,17 +23,50 @@ def download_svs(file_id, save_dir):
         output_file.write(response.content)
 
 
+def process_chunk(chunk, save_dir):
+    for row in chunk.itertuples():
+        file_id = row.id
+        try:
+            download_svs(file_id, save_dir)
+            with open(os.path.join(save_dir, "saved_ids.txt"), "a") as f:
+                f.write(f"{file_id}\n")
+        except Exception as e:
+            print(f"Failed to download file {file_id} with error {e}")
+
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Download WSI files from GDC")
-    parser.add_argument("--save_dir", type=str, default="../wsi_files/BLCA", help="Directory to save WSI files")
-    parser.add_argument("--cleaned_csv_path", type=str, default="dataset_csv/tcga_blca_all_clean.csv", help="Path to cleaned CSV file")
-    parser.add_argument("--manifest_txt_path", type=str, default="dataset_csv/gdc_manifest.2025-11-08.140931.txt", help="Path to GDC manifest txt file")
-    
+    parser.add_argument(
+        "--save_dir",
+        type=str,
+        default="../wsi_files/BLCA",
+        help="Directory to save WSI files",
+    )
+    parser.add_argument(
+        "--cleaned_csv_path",
+        type=str,
+        default="dataset_csv/tcga_blca_all_clean.csv",
+        help="Path to cleaned CSV file",
+    )
+    parser.add_argument(
+        "--manifest_txt_path",
+        type=str,
+        default="dataset_csv/gdc_manifest.2025-11-08.140931.txt",
+        help="Path to GDC manifest txt file",
+    )
+    parser.add_argument(
+        "--num_cpus",
+        type=int,
+        default=os.cpu_count(),
+        help="Number of CPUs to use for downloading files",
+    )
+
     args = parser.parse_args()
     save_dir = args.save_dir
     cleaned_csv_path = args.cleaned_csv_path
     manifest_txt_path = args.manifest_txt_path
+    n_cpus = args.num_cpus
 
     os.makedirs(save_dir, exist_ok=True)
 
@@ -46,12 +77,22 @@ if __name__ == "__main__":
     selected_manifest = manifest_file[
         manifest_file["filename"].isin(slide_ids)
     ]  # filter manifest to only include files in slide_ids
-    for row in tqdm(selected_manifest.itertuples(), desc="Downloading WSI files"):
-        file_id = row.id
-        file_name = row.filename
-        try:
-            download_svs(file_id, save_dir)
-            with open(os.path.join(save_dir, "saved_ids.txt"), "a") as f:
-                f.write(f"{file_id}\n")
-        except Exception as e:
-            print(f"Failed to download file {file_id} with error {e}")
+    # divide the download task into n_cpus parts
+    chunks = np.array_split(selected_manifest, n_cpus)
+    # for row in tqdm(selected_manifest.itertuples(), desc="Downloading WSI files"):
+    #     file_id = row.id
+    #     file_name = row.filename
+    #     try:
+    #         download_svs(file_id, save_dir)
+    #         with open(os.path.join(save_dir, "saved_ids.txt"), "a") as f:
+    #             f.write(f"{file_id}\n")
+    #     except Exception as e:
+    #         print(f"Failed to download file {file_id} with error {e}")
+    processes = []
+    for chunk in chunks:
+        process = Process(target=process_chunk, args=(chunk, save_dir))
+        process.start()
+        processes.append(process)
+    for process in processes:
+        process.join()
+    print("All downloads completed.")
