201d0cb0654c207570fa2e1023cad34b9fe73d8d
riez <bao.thcs20@gmail.com>
2025-11-17 13:07:16 +0800

    feat: fix plot function, add stratify to data

--- lib/plot.py
+++ lib/plot.py
@@ -5,18 +5,20 @@ import os
 import argparse
 
 
-def plot_training_curves(current_path, result_logs, y_lim=(2, 5)):
+def plot_training_curves(current_path, result_logs, y_lim=None, num_cols=2):
     number_of_configs = len(result_logs)
-    num_cols = 2
     num_rows = number_of_configs // num_cols + number_of_configs % num_cols
 
     fig, axes = plt.subplots(
-        figsize=(16, 8),
+        figsize=(16, 4 * num_rows),
         ncols=num_cols,
         nrows=num_rows,
     )
+    print(num_rows, num_cols)
     for i in range(num_rows):
         for j in range(num_cols):
+            if i * num_cols + j >= number_of_configs:
+                break
             avg_losses = result_logs[i * num_cols + j].get("avg_losses")
             val_losses = result_logs[i * num_cols + j].get("val_losses")
             test_c_index = result_logs[i * num_cols + j].get("test_c_index")
@@ -28,24 +30,72 @@ def plot_training_curves(current_path, result_logs, y_lim=(2, 5)):
             lr = config.get("lr")
             weight_decay = config.get("weight_decay")
             num_epoch = config.get("num_epoch")
-            axes[i, j].plot(range(1, num_epoch + 1), avg_losses, marker="o")
-            axes[i, j].plot(range(1, num_epoch + 1), val_losses, marker="o")
-            axes[i, j].set_title(
-                f"Optimizer: {optimizer}, LR: {lr:.4f}, weight_decay: {weight_decay}, Test C-index: {test_c_index:.4f}, Train C-index: {train_c_index:.4f}",
-                size=10,
-                pad=10,
-            )
-            axes[i, j].set_xlabel("Epoch")
-            axes[i, j].set_ylim(y_lim)
-            axes[i, j].set_yticks(np.arange(y_lim[0], y_lim[1] + 0.1, 0.5))
-            axes[i, j].set_ylabel("Average Loss")
-            axes[i, j].grid()
-            axes[i, j].legend(["Train", "Validation"])
+            if type(axes) is np.ndarray:
+                # if axes is 2D array
+                if axes.ndim == 2:
+                    axes[i, j].plot(
+                        range(1, len(avg_losses) + 1), avg_losses, marker="o"
+                    )
+                    axes[i, j].plot(
+                        range(1, len(val_losses) + 1), val_losses, marker="o"
+                    )
+                    axes[i, j].set_title(
+                        f"Optimizer: {optimizer}, LR: {lr:.4f}, weight_decay: {weight_decay}, Test C-index: {test_c_index:.4f}, Train C-index: {train_c_index:.4f}",
+                        size=10,
+                        pad=10,
+                    )
+                    axes[i, j].set_xlabel("Epoch")
+                    if y_lim is not None:
+                        axes[i, j].set_ylim(y_lim)
+                        axes[i, j].set_yticks(
+                            np.arange(
+                                y_lim[0], y_lim[1] + 0.1, (y_lim[1] - y_lim[0]) / 10
+                            )
+                        )
+                    axes[i, j].set_ylabel("Average Loss")
+                    axes[i, j].grid()
+                    axes[i, j].legend(["Train", "Validation"])
+                elif axes.ndim == 1:  # if axes is 1D array
+                    axes[j].plot(range(1, len(avg_losses) + 1), avg_losses, marker="o")
+                    axes[j].plot(range(1, len(val_losses) + 1), val_losses, marker="o")
+                    axes[j].set_title(
+                        f"Optimizer: {optimizer}, LR: {lr:.4f}, weight_decay: {weight_decay}, Test C-index: {test_c_index:.4f}, Train C-index: {train_c_index:.4f}",
+                        size=10,
+                        pad=10,
+                    )
+                    axes[j].set_xlabel("Epoch")
+                    if y_lim is not None:
+                        axes[j].set_ylim(y_lim)
+                        axes[j].set_yticks(
+                            np.arange(
+                                y_lim[0], y_lim[1] + 0.1, (y_lim[1] - y_lim[0]) / 10
+                            )
+                        )
+                    axes[j].set_ylabel("Average Loss")
+                    axes[j].grid()
+                    axes[j].legend(["Train", "Validation"])
+            else:  # if there's only one plot
+                axes.plot(range(1, len(avg_losses) + 1), avg_losses, marker="o")
+                axes.plot(range(1, len(val_losses) + 1), val_losses, marker="o")
+                axes.set_title(
+                    f"Optimizer: {optimizer}, LR: {lr:.4f}, weight_decay: {weight_decay}, Test C-index: {test_c_index:.4f}, Train C-index: {train_c_index:.4f}",
+                    size=10,
+                    pad=10,
+                )
+                axes.set_xlabel("Epoch")
+                if y_lim is not None:
+                    axes.set_ylim(y_lim)
+                    axes.set_yticks(
+                        np.arange(y_lim[0], y_lim[1] + 0.1, (y_lim[1] - y_lim[0]) / 10)
+                    )
+                axes.set_ylabel("Average Loss")
+                axes.grid()
+                axes.legend(["Train", "Validation"])
     fig.tight_layout()
     plt.savefig(os.path.join(current_path, "training_curves.png"))
 
 
-def plot_top_configs(experiment_path: str, y_lim=(2, 5)):
+def plot_top_configs(experiment_path: str, y_lim=None, top_k: int = 2):
     log_path = os.path.join(experiment_path, "result_logs.jsonl")
     with open(log_path, "r") as f:
         result_logs = [json.loads(line) for line in f.readlines()]
@@ -53,9 +103,9 @@ def plot_top_configs(experiment_path: str, y_lim=(2, 5)):
             log for log in result_logs if log.get("test_c_index") is not None
         ]
         result_logs = sorted(result_logs, key=lambda x: x["test_c_index"], reverse=True)
-        # select top 4 results
-        top_4 = result_logs[:4]
-        plot_training_curves(experiment_path, top_4, y_lim=y_lim)
+        # select top 2 results
+        top_k_results = result_logs[:top_k]
+        plot_training_curves(experiment_path, top_k_results, y_lim=y_lim)
 
 
 if __name__ == "__main__":
@@ -73,15 +123,20 @@ if __name__ == "__main__":
         "--y_lim",
         type=float,
         nargs=2,
-        default=(2, 5),
+        default=None,
         help="y-axis limits for the loss plots",
+    )
+    parser.add_argument(
+        "--top_k",
+        type=int,
+        default=2,
+        help="number of top configurations to plot",
     )
     parser = parser.parse_args()
 
     plot_top_configs(
-        experiment_path=parser.experiment_path,
-        y_lim=parser.y_lim
+        experiment_path=parser.experiment_path, y_lim=parser.y_lim, top_k=parser.top_k
     )
 
 # example command to run:
-# python lib/plot.py --experiment_path ./experiments --y_lim 2 5
+# python lib/plot.py --experiment_path ./experiments --y_lim 2 5 --top_k 2
